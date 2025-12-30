b9a0aff142fdf07b65e5d50fe124cfa89cc36567
GirishMKulkarni <girishk100@gmail.com>
2025-12-30 21:52:16 +0530

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://parivartan.in',
+  site: 'https://niranjankulkarni.github.io',
+  base: '/niranjan_website',
   vite: {
     plugins: [tailwindcss()]
   },
